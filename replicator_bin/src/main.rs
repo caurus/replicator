90985e59c260901c1bd5b0356706a7bb9cf7d8c3
John Schmitt <jjshutout19@yahoo.com>
2024-07-25 13:21:11 -0500

    Added ability to take a file argument and parse the lines from said file

--- replicator_bin/src/main.rs
+++ replicator_bin/src/main.rs
@@ -1,5 +1,35 @@
-use replicator_lib as rl;
+//use replicator_lib as rl;
+use std::{ env, fs, io };
 
 fn main() {
-    rl::phw();
+    //rl::phw();
+
+    //TODO: Get file name from CLA
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    //handle error of invalid file
+    let contents_r: Result<String, io::Error> = fs::read_to_string(file_path);
+    let contents: String;
+
+    match contents_r {
+        Ok(val) => contents = val,
+        Err(_) => panic!("Invalid file!")
+    }
+
+    //TODO: Also consider posibility of trouble reading file
+
+
+    //TODO: splice the long string by \n to get all the VM names in a vec
+    let vm_names: Vec<String> = contents.lines().map(|x| x.to_string()).collect();
+
+    //TODO: Call lib func to spin up machine from vec holding VM names
+
+    //TODO: Check if binary is already installed
+    //          if not, install binary
+
+    //TODO: Run the binary
+
+    //TODO: Call lib func to spin down machine from vec holding VM names
 }
--- replicator_lib/src/lib.rs
+++ replicator_lib/src/lib.rs
@@ -3,3 +3,9 @@ pub fn phw(){
     print!("Hello World from lib.rs!");
 }
 
+
+//TODO: Func to spin up a machine given a VM name
+
+//TODO: Func to run a binary on said machine??
+
+//TODO: Func to spin down a machine given a VM name
