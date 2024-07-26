765447fda73d09828f9e343dd919157ec585eec5
John Schmitt <jjshutout19@yahoo.com>
2024-07-26 16:21:35 -0500

    Added password feature in main.rs, other than that just a commit before the weekend

--- replicator_bin/src/main.rs
+++ replicator_bin/src/main.rs
@@ -2,12 +2,12 @@
 use std::{ env, fs, io };
 
 fn main() {
-    //rl::phw();
 
     //TODO: Get file name from CLA
     let args: Vec<String> = env::args().collect();
 
     let file_path = &args[1];
+    let username = &args[2];
 
     //handle error of invalid file
     let contents_r: Result<String, io::Error> = fs::read_to_string(file_path);
@@ -15,8 +15,12 @@ fn main() {
 
     match contents_r {
         Ok(val) => contents = val,
-        Err(_) => panic!("Invalid file!")
+        Err(error) => panic!("Threw the following error: {}", error)
     }
+
+    //get password from user for VMs
+    let mut pass = String::new();
+    io::stdin().read_line(&mut pass).expect("Error reading input");
 
     //TODO: Also consider posibility of trouble reading file
 
@@ -24,7 +28,9 @@ fn main() {
     //TODO: splice the long string by \n to get all the VM names in a vec
     let vm_names: Vec<String> = contents.lines().map(|x| x.to_string()).collect();
 
-    //TODO: Call lib func to spin up machine from vec holding VM names
+    //TODO: Call lib func to connect to machine from vec holding VM names
+    // params: username, vm_name, pass
+    
 
     //TODO: Check if binary is already installed
     //          if not, install binary
--- replicator_lib/src/lib.rs
+++ replicator_lib/src/lib.rs
@@ -1,10 +1,22 @@
+//use std::error::Error;
 
-pub fn phw(){
-    print!("Hello World from lib.rs!");
+//things to help with connecting to vm
+
+
+
+
+//helper function to spin_up_machine
+//params: username - name of the user on the vm
+//        vm_ip - ip address of the vm we are trying to connect to
+//return: Result holding either a successful connection or the error thrown during the process
+
+ 
+
+pub fn connect_to_machine(username: String, vm_ip: String) -> Result<(), Box<dyn Error>> {
+
 }
 
 
-//TODO: Func to spin up a machine given a VM name
 
 //TODO: Func to run a binary on said machine??
 
